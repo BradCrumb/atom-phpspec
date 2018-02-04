504deeeef7537518fe631713bc1e0c11669484c5
Marc-Jan Barnhoorn <m.barnhoorn@xsarus.nl>
2018-02-04 20:11:42 +0100

    Initial version

---
+++ spec/atom-phpspec-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomPhpspecView from '../lib/atom-phpspec-view';
+
+describe('AtomPhpspecView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
